5766608dd229ca0cd3f2bc2c4fb9fbfcd177f22e
SamainJamoneria <samaincormelana@gmail.com>
2026-07-15 13:26:30 +0200

    Update menu.js

--- menu.js
+++ menu.js
@@ -1,51 +1,98 @@
 const carta = [
 
 {
+    id:"tostas",
+    titulo:"Tostas Premium",
+    icono:"🥖",
+    productos:[
 
-nombre:"🥖 Tostas Premium",
+        {
+            nombre:"Burrata con polvo de jamón ibérico",
+            descripcion:"Tomate, burrata, AOVE, pesto y polvo de jamón ibérico",
+            precio:"12,50 €"
+        },
 
-id:"tostas",
+        {
+            nombre:"Chicharrones con queso del país",
+            descripcion:"",
+            precio:"11,00 €"
+        },
 
-productos:[
+        {
+            nombre:"Cecina con gorgonzola",
+            descripcion:"Tomate, cecina, queso gorgonzola y ralladura de parmigiano",
+            precio:"12,00 €"
+        },
 
-{
-nombre:"Burrata con polvo de jamón ibérico",
-descripcion:"Burrata cremosa acompañada de polvo de jamón ibérico.",
-precio:"12,50 €"
+        {
+            nombre:"Jamón con parmesano y aceite Isbilya",
+            descripcion:"Tomate, jamón y ralladura de parmigiano",
+            precio:"10,00 €"
+        },
+
+        {
+            nombre:"Anchoas con queso del país",
+            descripcion:"",
+            precio:"12,00 €"
+        },
+
+        {
+            nombre:"Vegetal",
+            descripcion:"Queso crema, guacamole, mermelada de tomate, tomate cherry y semillas de amapola",
+            precio:"11,00 €"
+        }
+
+    ]
+
 },
 
 {
-nombre:"Tosta de Cecina",
-descripcion:"",
-precio:"12,00 €"
-}
+    id:"tablas",
+    titulo:"Tablas",
+    icono:"🧀",
+    productos:[
 
-]
+        {
+            nombre:"Samaín",
+            descripcion:"Chorizo ibérico, salchichón ibérico, jamón de Granada, lomo ibérico y tres quesos a elegir",
+            precio:"23,00 €"
+        },
+
+        {
+            nombre:"Ibérica",
+            descripcion:"Jamón ibérico, lomo ibérico, chorizo ibérico, salchichón ibérico y cecina",
+            precio:"25,00 €"
+        },
+
+        {
+            nombre:"Quesos",
+            descripcion:"Cinco tipos de quesos diferentes con sus respectivos maridajes",
+            precio:"23,50 €"
+        }
+
+    ]
 
 },
 
 {
+    id:"raciones",
+    titulo:"Raciones",
+    icono:"🍖",
+    productos:[
 
-nombre:"🧀 Tablas",
+        {nombre:"Lomo ibérico",descripcion:"Media / Entera",precio:"8,00 € / 15,00 €"},
+        {nombre:"Salchichón ibérico",descripcion:"Media / Entera",precio:"7,00 € / 14,00 €"},
+        {nombre:"Chorizo ibérico",descripcion:"Media / Entera",precio:"7,00 € / 14,00 €"},
+        {nombre:"Jamón Duroc 100%",descripcion:"Media / Entera",precio:"8,00 € / 15,00 €"},
+        {nombre:"Jamón D.O. Granada",descripcion:"Media / Entera",precio:"7,00 € / 14,00 €"},
+        {nombre:"Cecina D.O. León",descripcion:"Media / Entera",precio:"9,00 € / 16,00 €"},
+        {nombre:"Chicharrones con queso del país",descripcion:"Media / Entera",precio:"6,50 € / 13,00 €"},
+        {nombre:"Ensalada de Ventresca",descripcion:"Con reducción de PX",precio:"14,00 €"},
+        {nombre:"Ensalada cherry",descripcion:"Tomates cherry con pesto casero y parmigiano",precio:"8,00 €"},
+        {nombre:"Ensalada piquillo",descripcion:"Pimientos del piquillo con anchoas y queso del país",precio:"14,50 €"},
+        {nombre:"Boquerones en vinagre",descripcion:"",precio:"10,00 €"},
+        {nombre:"Gildas",descripcion:"Precio por unidad",precio:"2,70 €"}
 
-id:"tablas",
+    ]
 
-productos:[
-
-{
-nombre:"Tabla Ibérica",
-descripcion:"",
-precio:"18,00 €"
 },
-
-{
-nombre:"Tabla de Quesos",
-descripcion:"",
-precio:"16,00 €"
-}
-
-]
-
-}
-
-];
