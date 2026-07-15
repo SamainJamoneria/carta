04ffd17316ab31a3c25c4c4864c97eb1bcc7cdb2
SamainJamoneria <samaincormelana@gmail.com>
2026-07-15 12:44:50 +0200

    Update script.js

--- script.js
+++ script.js
@@ -32,4 +32,42 @@ const carta = {
 
     ]
 
+    function crearProducto(producto){
+
+    return `
+
+    <div class="producto">
+
+        <div class="nombre">${producto.nombre}</div>
+
+        <div class="descripcion">${producto.descripcion}</div>
+
+        <div class="precio">${producto.precio}</div>
+
+    </div>
+
+    `;
+
+}
+
+function cargarCategoria(id,categoria){
+
+    const contenedor=document.getElementById(id);
+
+    categoria.forEach(producto=>{
+
+        contenedor.innerHTML+=crearProducto(producto);
+
+    });
+
+}
+
+document.addEventListener("DOMContentLoaded",()=>{
+
+    cargarCategoria("lista-tostas",carta.tostas);
+
+    cargarCategoria("lista-tablas",carta.tablas);
+
+});
+
 };
