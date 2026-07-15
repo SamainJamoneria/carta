b6d04787c27ab2e7698dd188b26c6c04f968ecba
SamainJamoneria <samaincormelana@gmail.com>
2026-07-15 13:12:23 +0200

    Update menu.js

--- menu.js
+++ menu.js
@@ -1,35 +1,51 @@
-const carta = {
+const carta = [
 
-    tostas: [
+{
 
-        {
-            nombre: "Burrata con polvo de jamón ibérico",
-            descripcion: "Burrata cremosa acompañada de polvo de jamón ibérico.",
-            precio: "12,50 €"
-        },
+nombre:"🥖 Tostas Premium",
 
-        {
-            nombre: "Tosta de Cecina",
-            descripcion: "",
-            precio: "12,00 €"
-        }
+id:"tostas",
 
-    ],
+productos:[
 
-    tablas: [
+{
+nombre:"Burrata con polvo de jamón ibérico",
+descripcion:"Burrata cremosa acompañada de polvo de jamón ibérico.",
+precio:"12,50 €"
+},
 
-        {
-            nombre: "Tabla Ibérica",
-            descripcion: "",
-            precio: "18,00 €"
-        },
+{
+nombre:"Tosta de Cecina",
+descripcion:"",
+precio:"12,00 €"
+}
 
-        {
-            nombre: "Tabla de Quesos",
-            descripcion: "",
-            precio: "16,00 €"
-        }
+]
 
-    ]
+},
 
-};
+{
+
+nombre:"🧀 Tablas",
+
+id:"tablas",
+
+productos:[
+
+{
+nombre:"Tabla Ibérica",
+descripcion:"",
+precio:"18,00 €"
+},
+
+{
+nombre:"Tabla de Quesos",
+descripcion:"",
+precio:"16,00 €"
+}
+
+]
+
+}
+
+];
