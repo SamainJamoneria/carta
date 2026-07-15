4654ea61b3793a580cc1665597dae3c08d039757
SamainJamoneria <samaincormelana@gmail.com>
2026-07-15 12:43:02 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,35 @@
-// =========================
-// Carta Samaín La Cormelana
-// =========================
+const carta = {
 
-document.addEventListener("DOMContentLoaded", () => {
+    tostas: [
 
-    console.log("Carta cargada correctamente");
+        {
+            nombre: "Burrata con polvo de jamón ibérico",
+            descripcion: "Burrata cremosa acompañada de polvo de jamón ibérico.",
+            precio: "12,50 €"
+        },
 
-});
+        {
+            nombre: "Tosta de Cecina",
+            descripcion: "",
+            precio: "12,00 €"
+        }
+
+    ],
+
+    tablas: [
+
+        {
+            nombre: "Tabla Ibérica",
+            descripcion: "",
+            precio: "18,00 €"
+        },
+
+        {
+            nombre: "Tabla de Quesos",
+            descripcion: "",
+            precio: "16,00 €"
+        }
+
+    ]
+
+};
