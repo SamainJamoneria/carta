a165df1604e7ab460f6296706ea246926b56e420
SamainJamoneria <samaincormelana@gmail.com>
2026-07-16 10:49:54 +0200

    Update menu-en.js

--- menu-en.js
+++ menu-en.js
@@ -1,1 +1,283 @@
+const carta = [
 
+{
+    id:"tostas",
+    titulo:"Premium Toasts",
+    icono:"🥖",
+    productos:[
+
+        {
+            nombre:"Burrata with Iberian Ham Powder",
+            descripcion:"Tomato, burrata, extra virgin olive oil, homemade pesto and Iberian ham powder",
+            precio:"12,50 €"
+        },
+
+        {
+            nombre:"Chicharrones with Local Cheese",
+            descripcion:"",
+            precio:"11,00 €"
+        },
+
+        {
+            nombre:"Cecina with Gorgonzola",
+            descripcion:"Tomato, cecina, Gorgonzola cheese and grated Parmigiano",
+            precio:"12,00 €"
+        },
+
+        {
+            nombre:"Ham with Parmigiano & Isbilya EVOO",
+            descripcion:"Tomato, cured ham and grated Parmigiano",
+            precio:"10,00 €"
+        },
+
+        {
+            nombre:"Anchovies with Local Cheese",
+            descripcion:"",
+            precio:"12,00 €"
+        },
+
+        {
+            nombre:"Vegetarian",
+            descripcion:"Cream cheese, guacamole, tomato jam, cherry tomatoes and poppy seeds",
+            precio:"11,00 €"
+        }
+
+    ]
+
+},
+
+{
+    id:"tablas",
+    titulo:"Sharing Boards",
+    icono:"🍽️",
+    productos:[
+
+        {
+            nombre:"Samaín Sharing Board",
+            descripcion:"Iberian chorizo, Iberian salchichón, Granada cured ham, Iberian pork loin and three cheeses of your choice",
+            precio:"23,00 €"
+        },
+
+        {
+            nombre:"Iberian Sharing Board",
+            descripcion:"100% Iberian ham, Iberian pork loin, Iberian chorizo, Iberian salchichón and cecina",
+            precio:"25,00 €"
+        },
+
+        {
+            nombre:"Cheese Selection",
+            descripcion:"Five different cheeses served with carefully selected accompaniments",
+            precio:"23,50 €"
+        }
+
+    ]
+
+},
+
+{
+    id:"raciones",
+    titulo:"Sharing Plates",
+    icono:"🍖",
+    productos:[
+
+        {
+            nombre:"Iberian Pork Loin",
+            descripcion:"Half / Whole",
+            precio:"8,00 € / 15,00 €"
+        },
+
+        {
+            nombre:"Iberian Salchichón",
+            descripcion:"Half / Whole",
+            precio:"7,00 € / 14,00 €"
+        },
+
+        {
+            nombre:"Iberian Chorizo",
+            descripcion:"Half / Whole",
+            precio:"7,00 € / 14,00 €"
+        },
+
+        {
+            nombre:"100% Duroc Ham",
+            descripcion:"Half / Whole",
+            precio:"8,00 € / 15,00 €"
+        },
+
+        {
+            nombre:"Granada Cured Ham",
+            descripcion:"Half / Whole",
+            precio:"7,00 € / 14,00 €"
+        },
+
+        {
+            nombre:"Cecina (Dry-Cured Beef)",
+            descripcion:"Half / Whole",
+            precio:"9,00 € / 16,00 €"
+        },
+
+        {
+            nombre:"Chicharrones with Local Cheese",
+            descripcion:"Half / Whole",
+            precio:"6,50 € / 13,00 €"
+        },
+
+        {
+            nombre:"Tuna Belly Salad",
+            descripcion:"With Pedro Ximénez reduction",
+            precio:"14,00 €"
+        },
+
+        {
+            nombre:"Cherry Tomato Salad",
+            descripcion:"Cherry tomatoes with homemade pesto and Parmigiano",
+            precio:"8,00 €"
+        },
+
+        {
+            nombre:"Piquillo Pepper Salad",
+            descripcion:"Piquillo peppers with anchovies and local cheese",
+            precio:"14,50 €"
+        },
+
+        {
+            nombre:"Marinated Anchovies",
+            descripcion:"",
+            precio:"10,00 €"
+        },
+
+        {
+            nombre:"Gildas",
+            descripcion:"Price per unit",
+            precio:"2,70 €"
+        }
+
+    ]
+
+},
+
+{
+    id:"quesos",
+    titulo:"Cheeses",
+    icono:"🧀",
+    productos:[
+
+        {
+            nombre:"Old Sheep's Cheese",
+            descripcion:"",
+            precio:"7,00 €"
+        },
+
+        {
+            nombre:"Goat's Cheese",
+            descripcion:"",
+            precio:"7,00 €"
+        },
+
+        {
+            nombre:"Smoked Cheese",
+            descripcion:"",
+            precio:"7,00 €"
+        },
+
+        {
+            nombre:"Blue Cheese",
+            descripcion:"",
+            precio:"7,00 €"
+        },
+
+        {
+            nombre:"Truffled Cheese",
+            descripcion:"",
+            precio:"7,50 €"
+        }
+
+    ]
+
+},
+
+{
+    id:"bocadillos",
+    titulo:"Sandwiches",
+    icono:"🥖",
+    productos:[
+
+        {
+            nombre:"100% Iberian Ham",
+            descripcion:"",
+            precio:"12,00 €"
+        },
+
+        {
+            nombre:"Granada Cured Ham",
+            descripcion:"",
+            precio:"6,50 €"
+        },
+
+        {
+            nombre:"Iberian Salchichón",
+            descripcion:"",
+            precio:"5,00 €"
+        },
+
+        {
+            nombre:"Iberian Chorizo",
+            descripcion:"",
+            precio:"5,00 €"
+        },
+
+        {
+            nombre:"Iberian Pork Loin",
+            descripcion:"",
+            precio:"6,50 €"
+        },
+
+        {
+            nombre:"Cheese",
+            descripcion:"",
+            precio:"4,00 €"
+        },
+
+        {
+            nombre:"Mortadella",
+            descripcion:"",
+            precio:"4,50 €"
+        },
+
+        {
+            nombre:"Bacon",
+            descripcion:"",
+            precio:"5,00 €"
+        },
+
+        {
+            nombre:"Extra Ingredient",
+            descripcion:"",
+            precio:"0,50 €"
+        }
+
+    ]
+
+},
+
+{
+    id:"postres",
+    titulo:"Desserts",
+    icono:"🍰",
+    productos:[
+
+        {
+            nombre:"Cheese with Quince",
+            descripcion:"",
+            precio:"5,00 €"
+        },
+
+        {
+            nombre:"Ice Cream",
+            descripcion:"Ask for available flavours",
+            precio:"Check price"
+        }
+
+    ]
+
+},
