cfc34a166ee8cf3ae5c042a75ffcf83456d24b94
SamainJamoneria <samaincormelana@gmail.com>
2026-07-17 10:58:17 +0200

    Update destino.js

--- destino.js
+++ destino.js
@@ -4,6 +4,33 @@
 
 document.addEventListener("DOMContentLoaded", () => {
 
-    console.log("Destino Samaín iniciado");
+    const botonDestino = document.getElementById("btn-destino");
+    const modalDestino = document.getElementById("modal-destino");
+    const cerrarDestino = document.getElementById("cerrar-destino");
+
+    // Abrir ventana
+    botonDestino.addEventListener("click", () => {
+
+        modalDestino.classList.add("visible");
+
+    });
+
+    // Botón cerrar
+    cerrarDestino.addEventListener("click", () => {
+
+        modalDestino.classList.remove("visible");
+
+    });
+
+    // Cerrar pulsando fuera de la ventana
+    modalDestino.addEventListener("click", (e) => {
+
+        if(e.target === modalDestino){
+
+            modalDestino.classList.remove("visible");
+
+        }
+
+    });
 
 });
